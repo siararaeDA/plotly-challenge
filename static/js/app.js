39f308279ae335b8715d7b2972c81da47d9465ca
Siara Leininger <leinings@oregonstate.edu>
2021-01-20 18:45:52 -0800

    Populating dropdown.

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,21 @@ function getData(id) {
         console.log(metadata);
     });
 }
+// Initalize page
+function init() {
+    // Select the dropdown menu
+    var dropdown = d3.select("#selDataset")
+    // Read in the data
+    d3.json("../../data/samples.json").then((data) => {
+        console.log(data);
 
+        // Add the id's to the dropdown
+        data.names.forEach(function(bbID) {
+            dropdown.append("option").text(bbID).property("value");
+        });
+    });
+}
+
+init();
 getData();
   
